7531b2ead115bb3291f767edf4552453fc4884b0
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-01 16:11:41 -0700

    testing search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,7 +6,7 @@ use App\Post;
 use App\Page;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use data;
+
 
 
 class SearchController extends Controller {
@@ -14,15 +14,15 @@ class SearchController extends Controller {
     public function index(Request $request)
     {
 
-        $data = array(
-            'query' => $request->query
-
-
-        );
+//        $data = array(
+//            'query' => $request->query
+//
+//
+//        );
 //$data['query']
 
-        $searchedusers = DB::table('users')->where('name', 'LIKE', "$request->query")
-            ->orWhere('username', 'LIKE', "$request->query")->orWhere('email', 'LIKE', "$request->query")
+        $searchedusers = DB::table('users')->where('name', 'LIKE', $request->query)
+            ->orWhere('username', 'LIKE', $request->query)->orWhere('email', 'LIKE', $request->query)
             ->get();
 
         return view('results', ['searchedusers'=> $searchedusers]);
