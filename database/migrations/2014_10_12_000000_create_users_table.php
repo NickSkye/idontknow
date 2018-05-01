24ed2992388466d4dcbd1e756f67584a9a1934a6
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-01 08:59:08 -0700

    changed user db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('is_online');
+            $table->boolean('verified')->default(false);
+            $table->boolean('account_active');
             $table->rememberToken();
             $table->timestamps();
         });
