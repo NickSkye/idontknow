551228c48c4a507125b183fa354808ca6a8542db
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-01 15:59:55 -0700

    testing search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,22 @@
             <div class="col-md-10 col-sm-12 no-padding">
                 <div class="card">
                     <div class="card-header">
-                    
+
                     <form action="/search" method="post">
                         {{ csrf_field() }}
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="row">
-                                    <div class="col-sm-12">
+                                    <div class="col-sm-6">
                                         <div class="form-group">
                                             <input type="text" class="form-control" id="query" name="query" value="{{ old('query') }}" required>
                                         </div>
+                                    </div>
 
 
-
+                                    <div class="col-sm-6">
                                         <input type="submit" value="Submit" class="send"/>
+                                    </div>
                                     </div>
                                 </div>
                             </div>
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,15 +1,6 @@
 <h1>Search results</h1>
 
 
-
-    <h3>Pages:</h3>
-
-
-
-
-
-@if (count($searchedusers))
-
     <h3>Users:</h3>
 
     <ul>
@@ -22,4 +13,3 @@
 
     </ul>
 
-@endif
