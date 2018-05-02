82dbf86db780fb2104a05a2222070065f5426900
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-02 15:40:22 -0700

    fixed update or insert on user info

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,9 +35,9 @@ class HomeController extends Controller
 
 //        this portion sends the link to the db
 //        Storage::disk('s3')->get($storagePath).
-        if(!DB::table('profileinfo')->where('username', Auth::user()->username)->first()->exists() ) {
+        if(is_null(DB::table('profileinfo')->where('username', Auth::user()->username)->first()) ) {
             $imageName = "https://frendgrid.s3.us-west-1.amazonaws.com/profilepics/1525299219.png";
-            DB::table('profileinfo')->insert(['username' => Auth::user()->username, 'profileimage' => $imageName, 'aboutme' => "There's nothing here yet"]);
+            DB::table('profileinfo')->insert(['username' => Auth::user()->username, 'profileimage' => "https://frendgrid.s3.us-west-1.amazonaws.com/profilepics/1525299219.png", 'aboutme' => "There's nothing here yet"]);
 
         }
 
