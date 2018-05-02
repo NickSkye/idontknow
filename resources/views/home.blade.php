e4170530dad815879b249af491a25ac37fc28125
NickSkye <nickskye7@gmail.com>
2018-05-01 21:00:24 -0700

    megre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10 col-sm-12 no-padding">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Now let your friends know what you are up to!</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,9 @@
                         </div>
                     @endif
 
-                    Now let your friends know what you are up to!
+
+
+
 
 
 
