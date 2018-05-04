148a8ba948952e15b804025763f9bc4fbba4a910
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 16:04:32 -0700

    fixed search button

--- resources/views/partials/friendsearch.blade.php
+++ resources/views/partials/friendsearch.blade.php
@@ -4,7 +4,7 @@
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Search for frends..." id="query" name="query" value="{{ old('query') }}">
                         </div>
-                        <button type="submit" value="Submit" class="send"><i class="fa fa-search"></i></button>
+                        <span><button type="submit" value="Submit" class="send"><i class="fa fa-search"></i></button></span>
 
 
 
