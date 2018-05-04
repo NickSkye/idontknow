7836c1adf81d9f3b468cf971a86d0fe8d6420508
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 16:50:49 -0700

    fixed search button

--- resources/views/partials/permafooter.blade.php
+++ resources/views/partials/permafooter.blade.php
@@ -12,7 +12,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Create a new post</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -20,10 +20,7 @@
                 <div class="modal-body">
                    @include('partials.uploadimage')
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+
             </div>
         </div>
     </div>
