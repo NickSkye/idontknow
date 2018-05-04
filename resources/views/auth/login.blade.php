783948896e85f3a7a41af5915f7322972beba6b7
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 13:13:22 -0700

    removed username login for now

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,20 +25,20 @@
                             </div>
                         </div>
 
+{{--WILL CHANGE THIS LATER TO ALLOW USERNAME OR EMAIL TO BE USED TO LOGIN--}}
+                        {{--<div class="form-group row">--}}
+                            {{--<label for="username" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }}</label>--}}
 
-                        <div class="form-group row">
-                            <label for="username" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" autofocus>--}}
 
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                {{--@if ($errors->has('username'))--}}
+                                    {{--<span class="invalid-feedback">--}}
+                                        {{--<strong>{{ $errors->first('username') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
 
                         <div class="form-group row">
