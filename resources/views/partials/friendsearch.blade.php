6c7f919649a05b2474c3e54da529f18bc18d206a
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 15:55:54 -0700

    fixed search button

--- resources/views/partials/friendsearch.blade.php
+++ resources/views/partials/friendsearch.blade.php
@@ -1,21 +1,23 @@
-<form action="/search" method="post">
-    {{ csrf_field() }}
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="row">
-                <div class="col-xs-8 col-sm-6">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search for frends..." id="query" name="query" value="{{ old('query') }}">
+<div class="center-top-search">
+    <form action="/search" method="post">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-xs-8 col-sm-6">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search for frends..." id="query" name="query" value="{{ old('query') }}">
+                        </div>
                     </div>
-                </div>
 
 
-                <div class="col-xs-4 col-sm-6">
-                    <input type="submit" value="Submit" class="send"/>
+                    <div class="col-xs-4 col-sm-6">
+                        <button type="submit" value="Submit" class="send"><i class="fa fa-search"></i></button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
-</form>
+    </form>
+</div>
