982dbb6afb589abc91de98e04e3c90b59ce1780b
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 16:46:24 -0700

    fixed search button

--- resources/views/partials/permafooter.blade.php
+++ resources/views/partials/permafooter.blade.php
@@ -1,25 +1,30 @@
 <div class="permafooter">
 
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#uploadImage"><i class="fa fa-plus" aria-hidden="true"></i></button>
 
 
-    <div class="modal fade" id="uploadImage" role="dialog">
-        <div class="modal-dialog">
 
-            <!-- Modal content-->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadImage">
+        <i class="fa fa-plus" aria-hidden="true"></i>
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Modal Header</h4>
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
-                    @include('partials.uploadimage')
+                   @include('partials.uploadimage')
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
-
         </div>
     </div>
 
