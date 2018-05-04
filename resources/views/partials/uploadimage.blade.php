553d9f3ced6687312920157a5205b02de0ba3206
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-04 16:27:44 -0700

    fixed search button

--- resources/views/partials/uploadimage.blade.php
+++ resources/views/partials/uploadimage.blade.php
@@ -31,8 +31,11 @@
             <form action="{{ url('s3-image-upload') }}" enctype="multipart/form-data" method="POST">
                 {{ csrf_field() }}
                 <div class="row">
-                    <div class="col-md-12">
-                        <input type="file" name="image" />
+                    <div class="col-md-12 image-upload">
+                        <label for="file-input">
+                            <img src="http://goo.gl/pB9rpQ"/>
+                        </label>
+                        <input id="file-input" type="file" name="image" />
                     </div>
                     <div class="col-md-12">
                         <textarea rows="3" cols="40" placeholder="Tell your frends about your post..." type="text" name="description" ></textarea>
