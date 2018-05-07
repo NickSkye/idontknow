bb3efdc73c3bc5f94a8406958bb33768dd0b2b29
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-07 16:07:26 -0700

    added notification framework

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -13,6 +13,11 @@ class CommentsController extends Controller
         DB::table('comments')->insert(
             ['username' => Auth::user()->username, 'post_id' => $request->post_id, 'comment' => $request->comment, 'likes' => 0, 'dislikes' => 0, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
         );
+
+        $user = DB::table('posts')->where('id', $request->post_id)->value('username');
+        DB::table('notifications')->insert(
+            ['username' => $user, 'notification' => 'New comment on your post ', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
+        );
 //
 //        // $pages = Page::where('title', 'LIKE', "%$query%")->get();
 //
