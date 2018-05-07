ca90f52f1d2833533a61953f922da84882416edc
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-07 09:22:16 -0700

    footer work

--- resources/views/partials/permafooter.blade.php
+++ resources/views/partials/permafooter.blade.php
@@ -6,9 +6,17 @@
     </div>
 
     <div class="footer-button">
+
+    </div>
+
+    <div class="footer-button">
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadImage">
             <i class="fa fa-plus" aria-hidden="true"></i>
         </button>
+    </div>
+
+    <div class="footer-button">
+
     </div>
 
     <div class="footer-button">
