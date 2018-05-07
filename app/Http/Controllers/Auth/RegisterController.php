0bae086a9abc168a627be3a838cb0368ff5b3c1f
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-07 09:17:24 -0700

    footer work

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -58,7 +58,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'username' => 'required|string|max:255|unique:users',
-            'email' => 'required|string|email|max:255|unique:users|regex:/(.*)@(.*)\.edu/i',
+            'email' => 'required|string|email|max:255|unique:users', //|regex:/(.*)@(.*)\.edu/i'
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
--- resources/views/partials/permafooter.blade.php
+++ resources/views/partials/permafooter.blade.php
@@ -1,13 +1,19 @@
 <div class="permafooter">
 
 
+    <div class="footer-button">
 
+    </div>
 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadImage">
-        <i class="fa fa-plus" aria-hidden="true"></i>
-    </button>
+    <div class="footer-button">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadImage">
+            <i class="fa fa-plus" aria-hidden="true"></i>
+        </button>
+    </div>
 
-    <!-- Modal -->
+    <div class="footer-button">
+
+    </div>
 
 
 </div>
