022e83adde9d6ba9d3750f9a1d2981be9f8abf99
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-07 15:00:18 -0700

    added achievements table

---
+++ app/Http/Controllers/AchievementsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AchievementsController extends Controller
+{
+    public function add()
+    {
+//        DB::table('follows')->insert(
+//            ['username' => Auth::user()->username, 'followsusername' => $username]
+//        );
+//
+//        // $pages = Page::where('title', 'LIKE', "%$query%")->get();
+//
+//        return redirect("home")->with('status', 'friend added');
+    }
+}
