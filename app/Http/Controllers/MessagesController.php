eb1f89f18017097605e0ffac4c4e78423de77942
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-08 15:32:38 -0700

    added messaging framework

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+    public function messages()
+    {
+
+        //if (Auth::check()) {
+        $messages = DB::table('messages')->where('username', Auth::user()->username)->get();
+
+        return view('messages', ['messages'=> $messages]);
+//        }
+//        else{
+//            return view('auth.login');
+//        }
+
+    }
 }
