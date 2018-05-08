1f279c798af45721f45a844e6451169aafd336ff
NickSkye <nickskye7@gmail.com>
2018-05-07 18:19:52 -0700

    testing notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,9 +76,15 @@
                             @else
                                 @if (Request::is('/'))
                                 <li class="nav-item dropdown">
+                                    @if (!$notifs->isEmpty())
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        <i class="fa fa-exclamation-circle" aria-hidden="true"></i> <span class="caret"></span>
+                                        <i class="fa fa-exclamation-circle" aria-hidden="true" style="color: red;"></i> <span class="caret"></span>
                                     </a>
+                                    @else
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            <i class="fa fa-exclamation-circle" aria-hidden="true"></i> <span class="caret"></span>
+                                        </a>
+                                        @endif
 
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach($notifs as $notif)
