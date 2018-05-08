66b05f18798b1e8ca15bc369f6215970c4141e79
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-08 12:42:39 -0700

    added color

--- resources/views/partials/commentfield.blade.php
+++ resources/views/partials/commentfield.blade.php
@@ -4,9 +4,8 @@
         <div class="col-12">
             {{ Form::hidden('post_id', $post->id) }}
             <textarea rows="2" cols="40" placeholder="Comment on this post..." type="text" name="comment" ></textarea>
-        </div>
-        <div class="col-12">
             <button type="submit" class="btn comment-button">Comment</button>
         </div>
+        
     </div>
 </form>
