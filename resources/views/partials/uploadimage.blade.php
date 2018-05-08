93e5d226dccf884bdc5060ff6ffda5e18fc5d1c7
NickSkye <nickskye7@gmail.com>
2018-05-07 21:39:48 -0700

    testing notifications

--- resources/views/partials/uploadimage.blade.php
+++ resources/views/partials/uploadimage.blade.php
@@ -14,17 +14,17 @@
                 {{ csrf_field() }}
                 <div class="row">
                     <div class="col-12">
-                        <textarea rows="4" cols="40" placeholder="Tell your frends about your post..." type="text" name="description" ></textarea>
+                        <textarea rows="4" cols="50" placeholder="Tell your frends about your post..." type="text" name="description" ></textarea>
                     </div>
                     <div class="col-6 offset-2 image-upload">
-                        <label for="file-input">
+                        <label for="file-input" style="float: right">
                             <img src="http://goo.gl/pB9rpQ"/>
                         </label>
                         <input id="file-input" type="file" name="image" />
                         <img src="" id="profile-img-tag" width="200px" />
                     </div>
 
-                    <div class="col-md-4">
+                    <div class="col-md-4" style="align-self: flex-end;">
                         <button type="submit" class="btn btn-success" style="float: right;">Upload</button>
                     </div>
                 </div>
