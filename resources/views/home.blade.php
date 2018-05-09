eef10bd5a3e26e50f0d3d4a3db5661c60e88431c
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-09 15:20:37 -0700

    layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,21 +25,21 @@
                             {{--@foreach($friends as $friend)--}}
 
                                 @foreach($allfriendsinfo as $infos)
-                                    @foreach($infos as $info)
+                                    {{--@foreach($infos as $info)--}}
 
 
-                                            @if($info->username == $friend->followsusername)
+                                            {{--@if($info->username == $friend->followsusername)--}}
                                                 {{--<img src="{{$info->profileimage}}" class="img-fluid img-there" alt="">--}}
-                                            <a href="/users/{{$friend->followsusername}}" class="col-4" style="max-width: 31.333333%; background-image: url('{{$info->profileimage}}'); padding-bottom: 31.33333333%; width: 100%; height: 100%; background-size: cover; background-repeat: no-repeat; margin: 1%;">
+                                            <a href="/users/{{$infos->followsusername}}" class="col-4" style="max-width: 31.333333%; background-image: url('{{$info->profileimage}}'); padding-bottom: 31.33333333%; width: 100%; height: 100%; background-size: cover; background-repeat: no-repeat; margin: 1%;">
 
                                                     <div class="frend-box">
-                                                        <p>{{$friend->followsusername}}</p>
+                                                        <p>{{$infos->followsusername}}</p>
                                                     </div>
 
                                             </a>
-                                            @endif
+                                            {{--@endif--}}
 
-                                    @endforeach
+                                    {{--@endforeach--}}
 
                                 @endforeach
 
