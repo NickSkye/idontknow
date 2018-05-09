ef5fbeec3a1d6b2318ac8122f1f29798d83bc070
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-09 09:51:18 -0700

    added shout

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -90,7 +90,7 @@
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#sendShout">Shout Back!</button>
-                                                        <button type="submit" class="btn btn-secondary pull-left" data-dismiss="modal">Close</button>
+                                                        <button type="submit" class="btn btn-secondary pull-left" >Close</button>
                                                     </div>
                                                     </form>
                                                 </div>
