15ee8e28708f33368052dca0c94872ddcc84ff67
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-09 15:55:50 -0700

    layout

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -79,7 +79,19 @@
                                                     </div>
                                                     <div class="card-footer">
                                                         <div>
-                                                            @include('partials.commentfield')
+                                                            <form action="{{ url('comment') }}" method="POST">
+                                                                {{ csrf_field() }}
+                                                                <div class="row">
+                                                                    <div class="col-12">
+                                                                        {{ Form::hidden('post_id', $friendspost->id) }}
+                                                                        <textarea rows="3" cols="40" placeholder="Comment on this post..." type="text" name="comment" style="width: 100%;"></textarea>
+
+                                                                    </div>
+                                                                    <div class="col-12">
+                                                                        <button type="submit" class="btn comment-button">Comment</button>
+                                                                    </div>
+                                                                </div>
+                                                            </form>
                                                         </div>
                                                     </div>
                                                 </div>
