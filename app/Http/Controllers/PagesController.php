d96cfa7d4ad30625c0d046d8e2932d55a5094467
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-10 14:01:25 -0700

    layout infinite scroll

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -53,7 +53,7 @@ class PagesController extends Controller
 
         $selfincluded = DB::table('posts')->where('username',  Auth::user()->username)->get();
 
-        $friends_info_full = DB::table('follows')->join('profileinfo', 'follows.followsusername', '=', 'profileinfo.username')->join('users', 'follows.followsusername', '=', 'users.username')->join('posts', 'follows.followsusername', '=', 'posts.username')->where('follows.username', Auth::user()->username)->unionAll($selfincluded)->orderBy('posts.created_at', 'desc')->paginate(10); //'posts.updated_at'
+        $friends_info_full = DB::table('follows')->join('profileinfo', 'follows.followsusername', '=', 'profileinfo.username')->join('users', 'follows.followsusername', '=', 'users.username')->join('posts', 'follows.followsusername', '=', 'posts.username')->where('follows.username', Auth::user()->username)->union($selfincluded)->orderBy('posts.created_at', 'desc')->paginate(10); //'posts.updated_at'
 
         return $friends_info_full;
     }
