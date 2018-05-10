dd3da7f5bd002e256b730b4a83db9e0885be11ca
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-10 11:29:56 -0700

    layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        $notifs = DB::table('notifications')->where([
+            ['username', Auth::user()->username],
+            ['seen', false],
+        ])->get();
+        view()->share('notifs', $notifs);
 
 
     }
