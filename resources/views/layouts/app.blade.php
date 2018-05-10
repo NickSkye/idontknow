e92540d97d052f7c599e48c88c831b45c9b3b027
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-10 08:57:48 -0700

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,21 +47,7 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="/js/jquery.jscroll.js"></script>
-        <script type="text/javascript">
-        $('ul.pagination').hide();
-        $(function() {
-            $('.infinite-scroll').jscroll({
-                autoTrigger: true,
-                loadingHtml: '<img class="center-block" src="/images/loading.gif" alt="Loading..." />',
-                padding: 0,
-                nextSelector: '.pagination li.active + li a',
-                contentSelector: 'div.infinite-scroll',
-                callback: function() {
-                    $('ul.pagination').remove();
-                }
-            });
-        });
-        </script>
+
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
