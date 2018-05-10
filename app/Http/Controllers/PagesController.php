751afef8245ea20995c8d56aad58bde38a3fa058
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-10 13:57:48 -0700

    layout infinite scroll

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -50,7 +50,10 @@ class PagesController extends Controller
     }
 
     public function getFriendsInfoWithPosts(){
-        $friends_info_full = DB::table('follows')->join('profileinfo', 'follows.followsusername', '=', 'profileinfo.username')->join('users', 'follows.followsusername', '=', 'users.username')->join('posts', 'follows.followsusername', '=', 'posts.username')->where('follows.username', Auth::user()->username)->orWhere('posts.username',  Auth::user()->username)->orderBy('posts.created_at', 'desc')->distinct()->paginate(10);; //'posts.updated_at'
+
+        $selfincluded = DB::table('posts')->where('username',  Auth::user()->username)->get();
+
+        $friends_info_full = DB::table('follows')->join('profileinfo', 'follows.followsusername', '=', 'profileinfo.username')->join('users', 'follows.followsusername', '=', 'users.username')->join('posts', 'follows.followsusername', '=', 'posts.username')->where('follows.username', Auth::user()->username)->orderBy('posts.created_at', 'desc')->union($selfincluded)->paginate(10); //'posts.updated_at'
 
         return $friends_info_full;
     }
