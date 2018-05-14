09264e2b084984913f41085f232e5b0ed72bfd00
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-14 10:02:20 -0700

    added activity layout

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -76,13 +76,13 @@
                                                             <form action="{{ url('comment') }}" method="POST">
                                                                 {{ csrf_field() }}
                                                                 <div class="row">
-                                                                    <div class="col-12">
+                                                                    <div class="col-9">
                                                                         {{ Form::hidden('post_id', $friendspost->id) }}
                                                                         <textarea rows="3" cols="40" placeholder="Comment on this post..." type="text" name="comment" style="width: 100%;"></textarea>
 
                                                                     </div>
-                                                                    <div class="col-12">
-                                                                        <button type="submit" class="btn comment-button">Comment</button>
+                                                                    <div class="col-3">
+                                                                        <button type="submit" class="btn comment-button"><i class="fa fa-2x fa-paper-plane" aria-hidden="true"></i></button>
                                                                     </div>
                                                                 </div>
                                                             </form>
