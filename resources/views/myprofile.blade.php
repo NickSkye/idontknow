7f53f56a3f3d49edb01d149af9cbd9b37cc0450f
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-14 12:13:15 -0700

    formatting dates

--- resources/views/myprofile.blade.php
+++ resources/views/myprofile.blade.php
@@ -68,7 +68,7 @@
                         <div>
 
                             {{--my posts--}}
-                            <div class="row frend-area">
+                            <div class="row frend-area infinite-scroll">
                                 @foreach($myposts as $post)
                                     {{--<a href="/post/{{$post->id}}">--}}
                                         {{--<div class="frend-post-box">--}}
@@ -82,6 +82,7 @@
 
 
                                 @endforeach
+                                    {{ $myposts->links() }}
                             </div>
 
                         </div>
