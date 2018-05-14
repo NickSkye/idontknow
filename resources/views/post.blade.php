70f1102e3b9ba4256c89d97799f2561aff68530c
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-14 10:35:43 -0700

    added posts layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,21 +12,22 @@
 
 
 
-                            @if($post->username === Auth::user()->username)
-                                <form action="/delete-post/{{$post->id}}" enctype="multipart/form-data" method="POST">
-                                    {{ csrf_field() }}
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <button type="submit" class="delete-post-button" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="Delete Your Post? There's No Going Back!"><i
-                                                        class="fa fa-trash fa-2x"></i></button>
-                                        </div>
+                        @if($post->username === Auth::user()->username)
+                            <form action="/delete-post/{{$post->id}}" enctype="multipart/form-data" method="POST">
+                                {{ csrf_field() }}
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="delete-post-button" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="Delete Your Post? There's No Going Back!">
+                                            <i
+                                                    class="fa fa-trash fa-2x"></i></button>
                                     </div>
-                                </form>
-                                @else
+                                </div>
+                            </form>
+                        @else
                             <div class="pull-right">
                                 <a href="/">report</a>
                             </div>
-                            @endif
+                        @endif
 
 
                     </div>
@@ -41,86 +42,84 @@
                             {{--friends posts--}}
                             <div class="row frend-area post-area">
 
-                                    {{--POST MODAL--}}
-                                    <div class="modal fade" id="postModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">{{$post->description}}</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <img src="{{$post->imagepath}}" class="img-fluid" alt="">
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                {{--POST MODAL--}}
+                                <div class="modal fade" id="postModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">{{$post->description}}</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <img src="{{$post->imagepath}}" class="img-fluid" alt="">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-                                        {{--HERE--}}
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                                    {{--HERE--}}
 
-                                        <div class="frend-post-box post-post">
-                                            <div class="post-post-header">
-                                                <a href="/users/{{$post->username}}"><div style=" background-image: url('{{$post->profileimage}}');  width: 50px; height: 50px; background-size: cover; background-repeat: no-repeat; margin-right: 20px;">
-                                                    </div></a>
-                                                <a href="/users/{{$post->username}}">
-                                                    <p>{{$post->username}}</p>
-                                                    <p>shared at: {{$post->created_at}}</p>
-                                                </a>
-                                            </div>
-                                            <p>{{$post->description}}</p>
-                                            <button type="button" class="btn " data-toggle="modal" data-target="#postModal">
+                                    <div class="frend-post-box post-post">
+                                        <div class="post-post-header">
+                                            <a href="/users/{{$post->username}}">
+                                                <div style=" background-image: url('{{$post->profileimage}}');  width: 50px; height: 50px; background-size: cover; background-repeat: no-repeat; margin-right: 20px;">
+                                                </div>
+                                            </a>
+                                            <a href="/users/{{$post->username}}">
+                                                <p>{{$post->username}}</p>
+                                                <p>shared at: {{$post->created_at}}</p>
+                                            </a>
+                                        </div>
+                                        <p>{{$post->description}}</p>
+                                        <button type="button" class="btn " data-toggle="modal" data-target="#postModal">
                                             <img src="{{$post->imagepath}}" class="img-fluid" alt="" style="max-height: 500px;">
-                                            </button>
-                                            <p class="post-data">views: {{$post->views}}</p>
+                                        </button>
+                                        <p class="post-data">views: {{$post->views}}</p>
 
-                                            {{--{{ $friend }}--}}
-                                        </div>
-
+                                        {{--{{ $friend }}--}}
                                     </div>
 
-                                    {{--THIS PAGE WILL BE AN INDIVIDUAL CLICKED ON POST WITH COMMENT SECTION AND VOTES --}}
+                                </div>
+
+                                {{--THIS PAGE WILL BE AN INDIVIDUAL CLICKED ON POST WITH COMMENT SECTION AND VOTES --}}
 
                             </div>
 
                         </div>
                         <div class="comment-section infinite-scroll">
                             @foreach($thecomments as $comment)
-                                <div >
+                                <div>
 
                                     <div class="row">
 
 
-                                        <div class="col-1" style="min-width: 85px;">
-                                            <a href="/users/{{$comment->username}}">
-                                            <img src="{{$comment->profileimage}}" alt="" style="width: 75px;">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="/users/{{$comment->username}}">
-                                            <p class="comment-username">{{$comment->username}}</p>
-                                            </a>
-                                        </div>
+                                        <a href="/users/{{$comment->username}}">
+                                            <div style=" background-image: url('{{$comment->profileimage}}');  width: 50px; height: 50px; background-size: cover; background-repeat: no-repeat; margin-right: 20px;">
+                                            </div>
+                                        </a>
 
-                                        <div class="col-4" style="max-width: 175px;">
-                                            <p class="pull-right">{{$comment->created_at}}</p>
-                                        </div>
+
+                                        <a href="/users/{{$comment->username}}">
+                                            <p class="comment-username">{{$comment->username}}</p>
+                                        </a>
+                                        <p class="comment">{{$comment->comment}}</p>
+
+
+                                        <p class="pull-right">{{$comment->created_at}}</p>
+
                                         <div class="" style="max-width: 100px; position: absolute; right: 10px;">
                                             <p class="pull-right"><a href="">report</a></p>
                                         </div>
 
                                     </div>
-                                    <div class="row">
-                                        <div class="offset-1 col-8">
-                                            <p class="comment">{{$comment->comment}}</p>
-                                        </div>
-                                    </div>
+                                    
                                     <div class="row">
                                         <div class="col-12">
 
@@ -139,7 +138,7 @@
                     </div>
                     <div class="card-footer">
 
-                            <div> @include('partials.commentfield')</div>
+                        <div> @include('partials.commentfield')</div>
 
                     </div>
                 </div>
