7e1b11e5eb25d0ed6be93ce67926b3294a45f937
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-14 13:33:01 -0700

    linkify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,9 +45,8 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="linkify.min.js"></script>
-        <script src="linkify-jquery.min.js"></script>
+
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="/js/jquery.jscroll.js"></script>
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -73,6 +72,8 @@
             $( "#datepicker" ).datepicker();
         } );
         </script>
+        <script src="linkify.min.js"></script>
+        <script src="linkify-jquery.min.js"></script>
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
