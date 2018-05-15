0504ede9ef892aed3065cfbacf388a3a48a45b87
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-15 10:30:23 -0700

    autocomplete

--- resources/views/partials/footerlinks.blade.php
+++ resources/views/partials/footerlinks.blade.php
@@ -8,9 +8,9 @@
     <a class="<?php if ($page == 'support') {
             echo 'on-page';
         } ?>" href="/support">Support</a>
-    <a class="<?php if ($page == 'donate') {
-            echo 'on-page';
-        } ?>" href="/donate">Donate</a>
+    {{--<a class="<?php if ($page == 'donate') {--}}
+            {{--echo 'on-page';--}}
+        {{--} ?>" href="/donate">Donate</a>--}}
     <a class="<?php if ($page == 'suggestions') {
             echo 'on-page';
         } ?>" href="/suggestions">Suggest</a>
