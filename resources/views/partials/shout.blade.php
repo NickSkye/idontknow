0d4ecce60619f44c4a848746bc437a6eb6e7b955
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-15 09:04:53 -0700

    autocomplete

--- resources/views/partials/shout.blade.php
+++ resources/views/partials/shout.blade.php
@@ -1,5 +1,6 @@
 {{--start trial upload--}}
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 <script type="text/javascript">
 
 
