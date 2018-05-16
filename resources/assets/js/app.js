87dccedebf055b4f45efff58de0bba0e345f195f
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-16 14:39:08 -0700

    friend page followers and following

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -144,11 +144,11 @@ $(document).ready(function(){
     });
 
 
-    $("#followerCollapser").click(function() {  //use a class, since your ID gets mangled
+    $(".collapse").click(function() {  //use a class, since your ID gets mangled
         $('#followingCollapse').removeClass('show');      //add the class to the clicked element
     });
 
-    $("#followingCollapser").click(function() {  //use a class, since your ID gets mangled
+    $(".collapse").click(function() {  //use a class, since your ID gets mangled
         $('#followerCollapse').removeClass('show');      //add the class to the clicked element
     });
 
