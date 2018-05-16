11a59e922fc66e1063aa81413e3773158b13c605
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-16 11:08:50 -0700

    remove modal after shout

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -106,7 +106,7 @@
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#sendShout">Shout Back!</button>
-                                                        <button type="submit" class="btn btn-secondary pull-left" >Close</button>
+                                                        <button type="submit" class="btn btn-secondary pull-left modal-button" >Close</button>
                                                     </div>
                                                     </form>
                                                 </div>
