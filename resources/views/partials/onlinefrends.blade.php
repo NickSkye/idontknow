3f7da142f9ce5a8a5ca27d6afdc3b1af3925aa3d
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-17 11:39:16 -0700

    online frends

--- resources/views/partials/onlinefrends.blade.php
+++ resources/views/partials/onlinefrends.blade.php
@@ -1,7 +1,8 @@
+<div class="toggleon-online-frends">
+    <i class="fa fa-arrows-h" aria-hidden="true"></i>
+</div>
 <div class="online-frends" style="position: fixed;">
-    <div class="toggleon-online-frends">
-        <i class="fa fa-arrows-h" aria-hidden="true"></i>
-    </div>
+
     <div class="card" style="width: 100%;">
         <div class="card-header">
             <span class="toggle-online-frends"><i class="fa fa-arrows-h" aria-hidden="true"></i></span>Online Frends
