f8fa1516fa6173f028d2a1da2291d90513b39c35
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-17 10:36:32 -0700

    online frends

--- resources/views/friendspage.blade.php
+++ resources/views/friendspage.blade.php
@@ -80,8 +80,33 @@
                             <div class="col-6">
 
 
+
+
+
                                 <h2>{{$item->name}}</h2>
-                                <h4>{{$item->username}}</h4>
+                                @foreach($online_frends as $frend)
+                                    {{--now {{Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))}}--}}
+                                    {{--carbon {{$frend->username}} {{Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s')}}--}}
+
+                                    @if($frend->username === $item->username)
+                                        @if(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(2)->format('Y-m-d H:i:s') )
+                                            <i class="fa fa-circle" style="color: lime;" aria-hidden="true"></i>
+                                        @elseif(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s') )
+                                            <i class="fa fa-circle" style="color: orange;" aria-hidden="true"></i>
+                                        @else
+                                            <i class="fa fa-circle" style="color: red;" aria-hidden="true"></i>
+                                        @endif
+                                        <h4>{{$item->username}}</h4>
+                                        @if($item->username ===  Auth::user()->username )
+                                            (you)
+                                        @endif
+                                    @endif
+
+
+
+
+
+                                @endforeach
 
                                 <div class="profile-stats" style="display: flex; margin-bottom: 30px;">
                                     <div style="text-align: center; width: 75px;">
