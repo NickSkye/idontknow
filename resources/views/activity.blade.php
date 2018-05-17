3a650e5482d818a2d85931e908a98a76c59178f8
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-17 09:57:17 -0700

    online frends

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -276,8 +276,10 @@
 
 
                             <li class="list-group-item">
-                                @if(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s') )
+                                @if(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(2)->format('Y-m-d H:i:s') )
                                     <i class="fa fa-circle" style="color: lime;" aria-hidden="true"></i>
+                                    @elseif(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s') )
+                                    <i class="fa fa-circle" style="color: orange;" aria-hidden="true"></i>
                                     @else
                                     <i class="fa fa-circle" style="color: red;" aria-hidden="true"></i>
                                 @endif
