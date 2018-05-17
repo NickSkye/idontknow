fec4e639d50a1cd3ba599df5c3c33213f9660c8e
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-17 09:30:55 -0700

    online frends

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -270,8 +270,8 @@
                         <ul class="list-group list-group-flush">
 
     @foreach($online_frends as $frend)
-now {{Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))}}
-                            carbon {{$frend->username}} {{Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s')}}
+{{--now {{Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))}}--}}
+                            {{--carbon {{$frend->username}} {{Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s')}}--}}
                                 @if(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s') )
 
 
