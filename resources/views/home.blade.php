fdd26b08f0e14fe82d5b61a987981bf13067b015
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-17 11:03:47 -0700

    online frends

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,28 @@
                                 <div class="row frend-area">
 
                                     @foreach($allfriendsinfo as $infos)
+
+                                        @foreach($online_frends as $frend)
+                                            {{--now {{Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))}}--}}
+                                            {{--carbon {{$frend->username}} {{Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s')}}--}}
+
+                                            @if($frend->username === $infos->followsusername)
+                                                @if(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(2)->format('Y-m-d H:i:s') )
+                                                    <i class="fa fa-circle" style="color: lime;" aria-hidden="true"></i>
+                                                @elseif(Carbon\Carbon::parse($now->format('Y-m-d H:i:s'))->format('Y-m-d H:i:s') < Carbon\Carbon::parse($frend->updated_at)->addMinutes(5)->format('Y-m-d H:i:s') )
+                                                    <i class="fa fa-circle" style="color: orange;" aria-hidden="true"></i>
+                                                @else
+                                                    <i class="fa fa-circle" style="color: red;" aria-hidden="true"></i>
+                                                @endif
+
+                                            @endif
+
+
+
+
+
+                                        @endforeach
+
                                         <a href="/users/{{$infos->followsusername}}" class="col-4 home-frends-images" style="background-image: url('{{$infos->profileimage}}');">
                                             <div class="frend-box">
                                                 <p>{{$infos->followsusername}}</p>
