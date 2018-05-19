11315c7fb8fe76f1bf40ea03cc41ca3df994fcdd
NickSkye <nickskye7@gmail.com>
2018-05-18 21:40:32 -0700

    like

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -110,9 +110,10 @@
                                             <form action="/like" method="post" id="like_form">
                                                 {{ csrf_field() }}
                                                 <input type="hidden" name="postid" value="{{$post->id}}"/>
-                                                <button class="post-data" type="submit" name="submit" value="Submit Form">
-                                                            <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
-                                                </button>
+                                                <label for="submit"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></label>
+                                                <input class="post-data" type="submit" name="submit" value="Submit Form"/>
+                                                            {{--<i class="fa fa-heart fa-2x" aria-hidden="true"></i>--}}
+                                                {{--</button>--}}
                                                 <div id="server-results"><!-- For server results --></div>
                                             </form>
 
