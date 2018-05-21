cdfcc24274438a1791092505a50cf9dfa4a2c99d
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-21 11:10:13 -0700

    fixed vote thing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -259,7 +259,7 @@ $("#dislike_form").submit(function(event){
 });
 
 $(".add_frend_button").click(function() {  //use a class, since your ID gets mangled
-    $(".add_frend_button").attr("disabled", true);
+    
     $('.add_frend_button').toggleClass("loading-button");     //add the class to the clicked element
     $('.add_frend_button i.fa').toggleClass("fa-user-plus");     //add the class to the clicked element
     $('.add_frend_button i.fa').toggleClass("fa-spinner fa-pulse fa-fw");
