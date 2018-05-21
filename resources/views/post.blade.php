0ace22ce8adf0057b5cd306b260ad37086efa197
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-21 09:49:39 -0700

    fixed vote thing

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -136,7 +136,7 @@
 
                                             </form>
                                             </div>
-                                         
+
                                             <p class="post-data"><a href="#"><i class="fa fa-comment-o fa-2x" aria-hidden="true"></i></a></p>
                                             <p class="post-data"><a href="#"><i class="fa fa-share-alt fa-2x" aria-hidden="true"></i></a></p>
 
