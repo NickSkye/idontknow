89fea3376ce1b160e34bc29046b52d250dbffecd
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-22 08:58:26 -0700

    fixed meta bar

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -24,13 +24,13 @@ class SearchController extends Controller {
 
         $location = DB::table('users')->select('latitude', 'longitude')->where('username', Auth::user()->username)->first();
         if(is_null($location->latitude) or is_null($location->longitude)){
-            if(is_null(Session::get('latitude')) or is_null(Session::get('longitude'))){
+            if(is_null(\Session::get('latitude')) or is_null(\Session::get('longitude'))){
                 $location->latitude = 0;
                 $location->longitude = 0;
         }
         else{
-            $location->latitude = Session::get('latitude');
-            $location->longitude = Session::get('longitude');
+            $location->latitude = \Session::get('latitude');
+            $location->longitude = \Session::get('longitude');
         }
 
         }
