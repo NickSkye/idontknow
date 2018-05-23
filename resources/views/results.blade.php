63248de748e1255d24b6885ac5ee84f86e7e19d7
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-23 12:01:31 -0700

    adding meta bar to activity screen

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -57,7 +57,7 @@
                                             </div>
                                             <div class="frend-box">
                                                 <p>{{$sug->username}}</p>
-                                                <p>{{$sug->distance}}</p>
+                                                <p>d: {{$sug->distance}}</p>
                                             </div>
                                         </a>
                                     @endforeach
