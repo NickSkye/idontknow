a913d2fa315050e12f755ea40222f6f09b2455f8
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-24 09:56:19 -0700

    image orientation test

--- app/Http/Controllers/S3ImageController.php
+++ app/Http/Controllers/S3ImageController.php
@@ -108,6 +108,7 @@ if ($request->hasFile('image')) {
     $imageName = time().'.'.$request->image->getClientOriginalExtension();
     $image = $request->file('image');
     $image = Image::make($image)->orientate();
+    $image = $image->stream();
     $t = Storage::disk('s3')->put("posts/".$imageName, file_get_contents($image->__toString()), 'public');
     $imageName = Storage::disk('s3')->url("posts/".$imageName);
 } else{
