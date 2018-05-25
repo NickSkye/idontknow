d79d63d1a0f514c70ea8a85dd037faa0dcf928df
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-25 14:32:29 -0700

    nearby page

--- public/js/app.js
+++ public/js/app.js
@@ -14293,7 +14293,7 @@ $(".activity-comment").submit(function (event) {
 });
 
 $(".frend-butt").click(function () {
-    $('.comment-field').append("penus".text());
+    $('.comment-field').val('penus');
 });
 
 var oldVal = "";
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -425,7 +425,7 @@ $(".activity-comment").submit(function(event){
 
 $(".frend-butt").click( function()
     {
-        $('.comment-field').append( "penus".text());
+        $('.comment-field').val('penus');
     }
 );
 
