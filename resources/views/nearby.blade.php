924b0654892e449fcd088af81c5f36e625e17c6a
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-25 11:47:09 -0700

    nearby page

--- resources/views/nearby.blade.php
+++ resources/views/nearby.blade.php
@@ -69,7 +69,7 @@
 
 
 
-                                                    {!! preg_replace('/@([\w\-]+)/', '<a href="/users/$1">$0</a>', $friendspost->description) !!}
+                                                    <p>{!! preg_replace('/@([\w\-]+)/', '<a href="/users/$1">$0</a>', $friendspost->description) !!}</p>
                                                     <a href="/post/{{$friendspost->id}}">
                                                         <img src="{{$friendspost->imagepath}}" class="img-fluid activity-image" alt="">
 
