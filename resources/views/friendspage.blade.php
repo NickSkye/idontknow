b6c3ae2901c3104d623ed4ac8cc49d6338fe717d
NickSkye <nickskye7@gmail.com>
2018-05-26 10:24:01 -0700

    fixed users own page

--- resources/views/friendspage.blade.php
+++ resources/views/friendspage.blade.php
@@ -39,7 +39,7 @@
         <div class="row justify-content-center">
             <div class="col-md-10 col-sm-12 no-padding">
                 <div class="card">
-                    <div class="card-header" style="padding: 0;">
+                    <div class="card-header" style="padding: 0; background-color: white;">
                         {{--@include('partials.friendsearch')--}}
 
 
