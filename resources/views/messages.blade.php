03dd828123551d7a384e72126126730b5ecb0c0d
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-29 10:16:31 -0700

    frend page

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -160,7 +160,7 @@
                                                     @if(Auth::user()->username === $oldmess->from_username)
                                                     <div class=" profile-image-shout-page "
                                                     style="background-image: url('{{Auth::user()->profileimage}}');"></div>
-                                                        <p style="margin-left: 1rem;">Shout from {{$oldmess->from_username}} <br> {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}</p>
+                                                        <p style="margin-left: 1rem;">Shout to {{$oldmess->username}} <br> {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}</p>
                                                     @endif
                                           <p>{{$oldmess->message}}</p>
                                             </div>
