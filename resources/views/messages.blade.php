9b4283f5d250b2cb4234c11fcaa1d83bbf447c13
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-29 12:04:35 -0700

    shouts sent

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -53,7 +53,7 @@
                     <div class="card-header">
                         <div class="row">
                             <div class="col-6">
-                        <h2>Shouts</h2>
+                                <h2>Shouts</h2>
                             </div>
                             <div class="col-6">
                                 @if($hasfriends)
@@ -64,7 +64,7 @@
                                 @else
 
                                     <p>You have to add frends before you can shout at them</p>
-                                    @endif
+                                @endif
                             </div>
                         </div>
                     </div>
@@ -75,63 +75,68 @@
                             </div>
                         @endif
 
-                            @if($hasfriends)
+                        @if($hasfriends)
 
 
-                                <button type="button" class="shout-fixed-button" data-toggle="modal" data-target="#sendShout">
-                                    <i class="fa fa-bullhorn fa-2x" aria-hidden="true"></i>
-                                </button>
+                            <button type="button" class="shout-fixed-button" data-toggle="modal" data-target="#sendShout">
+                                <i class="fa fa-bullhorn fa-2x" aria-hidden="true"></i>
+                            </button>
 
-                            @endif
+                        @endif
 
 
                         <div>
 
                             {{--friends posts--}}
                             <div class="row frend-area">
-                                @foreach($messages as $mess)
+                                @foreach($friends as $fr)
+                                {{--@foreach($messages as $mess)--}}
                                     {{--MODAL START--}}
 
-                                        <div class="modal fade" id="viewShout-{{$mess->id}}" tabindex="-1" role="dialog" aria-labelledby="viewshoutModalLabel-{{$mess->id}}" aria-hidden="true">
-                                            <div class="modal-dialog" role="document">
-                                                <div class="modal-content">
-                                                    <form action="/shouts/shoutseen" method="post">
-                                                        {{ csrf_field() }}
-                                                        <input type="hidden" name="shoutid" value="{{$mess->id}}"/>
-                                                        <input type="hidden" name="from_user" value="{{$mess->from_username}}"/>
-                                                        <input type="hidden" name="latitude" value=""/>
-                                                        <input  type="hidden" name="longitude" value=""/>
+                                    <div class="modal fade" id="viewShout-{{$fr->username}}" tabindex="-1" role="dialog" aria-labelledby="viewshoutModalLabel-{{$fr->username}}" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <form action="/shouts/shoutseen" method="post">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="shoutid" value="{{$fr->username}}"/>
+                                                    {{--<input type="hidden" name="from_user" value="{{$mess->from_username}}"/>--}}
+                                                    <input type="hidden" name="latitude" value=""/>
+                                                    <input type="hidden" name="longitude" value=""/>
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="viewshoutModalLabel-{{$mess->id}}">Shout!</h5>
+                                                        <h5 class="modal-title" id="viewshoutModalLabel-{{$fr->username}}">Shout!</h5>
                                                         <button type="submit" class="close" aria-label="Close">
                                                             <span aria-hidden="true">&times;</span>
                                                         </button>
                                                     </div>
                                                     <div class="modal-body">
                                                         {{--@include('partials.viewshout')--}}
-                                                        <p>Your friend {{$mess->from_username}} shouted:</p>
+                                                        <p>Your friend {{$fr->username}} shouted:</p>
+                                                        @foreach($messages as $mess)
                                                         <p>{{$mess->message}}</p>
                                                         <p>at: {{Carbon\Carbon::parse($mess->created_at)->format('d M Y g:i A')}}</p>
+                                                            @endforeach
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#sendShout">Shout Back!</button>
-                                                        <button type="submit" class="btn btn-secondary pull-left modal-button" >Close</button>
+                                                        <button type="submit" class="btn btn-secondary pull-left modal-button">Close</button>
                                                     </div>
-                                                    </form>
-                                                </div>
+                                                </form>
                                             </div>
                                         </div>
+                                    </div>
 
                                     {{--MODAL END--}}
                                     <div class="col-12">
 
-                                        <button type="button" class="btn " data-toggle="modal" data-target="#viewShout-{{$mess->id}}" style="display: flex; align-items: center; padding: 0; background: yellow; color: black; width: 100%;">
-                                           
+                                        <button type="button" class="btn " data-toggle="modal" data-target="#viewShout-{{$fr->username}}" style="display: flex; align-items: center; padding: 0; background: yellow; color: black; width: 100%;">
 
-                                                    <div class=" profile-image-shout-page "
-                                                         style="background-image: url('{{$mess->profileimage}}');"></div>
 
-                                            <p style="margin-left: 1rem;">Shout from {{$mess->from_username}} <br>received {{Carbon\Carbon::parse($mess->created_at)->format('d M Y g:i A')}}</p>
+                                            <div class=" profile-image-shout-page "
+                                                 style="background-image: url('{{$fr->profileimage}}');"></div>
+
+                                            <p style="margin-left: 1rem;">Shout from {{$fr->username}}
+                                                {{--<br>received {{Carbon\Carbon::parse($mess->created_at)->format('d M Y g:i A')}}--}}
+                                            </p>
                                         </button>
 
 
@@ -139,55 +144,59 @@
                                     </div>
 
                                 @endforeach
-                                    <hr>
-                                    @foreach($sentmessages as $sentmess)
-
-                                        <div class="col-12">
-
-                                            <div   style="display: flex; align-items: center;">
-
-
-                                                    <div class=" profile-image-shout-page old"
-                                                         style="background-image: url('{{$sentmess->profileimage}}');"></div>
-                                                    <p style="margin-left: 1rem;">Shout to {{$sentmess->username}} <br>sent {{Carbon\Carbon::parse($sentmess->updated_at)->diffForHumans()}}</p>
-
-
-
-
-                                                {{--<p>{{$oldmess->message}}</p>--}}
-                                            </div>
-
-
-                                            <hr>
-                                        </div>
-
-                                    @endforeach
                                 <hr>
-                                    @foreach($oldmessages as $oldmess)
+                                @foreach($sentmessages as $sentmess)
 
-                                        <div class="col-12">
+                                    <div class="col-12">
 
-                                            <div   style="display: flex; align-items: center;">
-
-                                                @if(Auth::user()->username === $oldmess->from_username)
-                                                        <div class=" profile-image-shout-page old"
-                                                             style="background-image: url('{{$oldmess->profileimage}}');"></div>
-                                                        <p style="margin-left: 1rem;">Shout to {{$oldmess->username}} <br>seen {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}</p>
-                                                        @elseif(Auth::user()->username === $oldmess->username)
-                                                            <div class=" profile-image-shout-page old" style="background-image: url('{{$oldmess->profileimage}}');"></div>
-                                                            <p style="margin-left: 1rem;">Shout from {{$oldmess->from_username}} <br>opened {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}</p>
-                                                @endif
+                                        <div style="display: flex; align-items: center;">
 
 
+                                            <div class=" profile-image-shout-page old"
+                                                 style="background-image: url('{{$sentmess->profileimage}}');"></div>
+                                            <p style="margin-left: 1rem;">Shout to {{$sentmess->username}}
+                                                <br>sent {{Carbon\Carbon::parse($sentmess->updated_at)->diffForHumans()}}
+                                            </p>
 
-                                          {{--<p>{{$oldmess->message}}</p>--}}
-                                            </div>
 
-
-                                            <hr>
+                                            {{--<p>{{$oldmess->message}}</p>--}}
                                         </div>
 
-                                    @endforeach
+
+                                        <hr>
+                                    </div>
+
+                                @endforeach
+                                <hr>
+                                @foreach($oldmessages as $oldmess)
+
+                                    <div class="col-12">
+
+                                        <div style="display: flex; align-items: center;">
+
+                                            @if(Auth::user()->username === $oldmess->from_username)
+                                                <div class=" profile-image-shout-page old"
+                                                     style="background-image: url('{{$oldmess->profileimage}}');"></div>
+                                                <p style="margin-left: 1rem;">Shout to {{$oldmess->username}}
+                                                    <br>seen {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}
+                                                </p>
+                                            @elseif(Auth::user()->username === $oldmess->username)
+                                                <div class=" profile-image-shout-page old" style="background-image: url('{{$oldmess->profileimage}}');"></div>
+                                                <p style="margin-left: 1rem;">Shout from {{$oldmess->from_username}}
+                                                    <br>opened {{Carbon\Carbon::parse($oldmess->updated_at)->diffForHumans()}}
+                                                </p>
+                                            @endif
+
+
+
+                                            {{--<p>{{$oldmess->message}}</p>--}}
+                                        </div>
+
+
+                                        <hr>
+                                    </div>
+
+                                @endforeach
                             </div>
 
                         </div>
@@ -197,7 +206,6 @@
                     <div class="card-footer">
                         <div>Shouts get deleted once viewed</div>
                     </div>
-
 
 
                 </div>
