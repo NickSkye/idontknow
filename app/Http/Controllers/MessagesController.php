bb4c0634f68e0fa94850f2adf18fcf2c4684c3bb
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-29 10:44:53 -0700

    frend page

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -39,9 +39,9 @@ class MessagesController extends Controller
     {
 
 
-        $messages = DB::table('messages')->join('profileinfo', 'messages.username', '=', 'profileinfo.username')->join('users', 'messages.username', '=', 'users.username')->where([['messages.username', Auth::user()->username], ['seen', false],])->orderBy('messages.created_at', 'desc')->get();
+        $messages = DB::table('messages')->join('profileinfo', 'messages.from_username', '=', 'profileinfo.username')->join('users', 'messages.from_username', '=', 'users.username')->where([['messages.username', Auth::user()->username], ['seen', false],])->orderBy('messages.created_at', 'desc')->get();
 
-        $oldmessages = DB::table('messages')->join('profileinfo', 'messages.username', '=', 'profileinfo.username')->join('users', 'messages.username', '=', 'users.username')->where([['messages.username', Auth::user()->username], ['seen', true],])->orWhere([['messages.from_username', Auth::user()->username],['seen', true],])->orderBy('messages.updated_at', 'desc')->limit(50)->get();
+        $oldmessages = DB::table('messages')->join('profileinfo', 'messages.from_username', '=', 'profileinfo.username')->join('users', 'messages.from_username', '=', 'users.username')->where([['messages.username', Auth::user()->username], ['seen', true],])->orWhere([['messages.from_username', Auth::user()->username],['seen', true],])->orderBy('messages.updated_at', 'desc')->limit(50)->get();
 
         $friends  = $this->getFriendsInfo(); //DB::table('follows')->where('username', Auth::user()->username)->get();
 
