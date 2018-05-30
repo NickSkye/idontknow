321dc53aa497c02c0f4224ac46ecb16381a4f176
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-30 11:18:28 -0700

    notification page accessible from mobile and desktop

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+<?php $page = 'about'; ?>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-sm-12 no-padding">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    {{--<img src="{{ Session::get('path') }}">--}}
+                @endif
+
+                {{--MODAL FOR SHOUTS--}}
+
+
+                <div class="modal fade" id="sendShout" tabindex="-1" role="dialog" aria-labelledby="sendshoutModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="sendshoutModalLabel">Shout!</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+
+                            </div>
+                            {{--<div class="modal-footer">--}}
+                            {{--<button type="button" class="btn btn-primary">Shout Back!</button>--}}
+                            {{--<button type="button" class="btn btn-secondary pull-left" data-dismiss="modal">Close</button>--}}
+                            {{--</div>--}}
+                        </div>
+                    </div>
+                </div>
+
+                {{--END MODALS FOR SHOUTS--}}
+
+                <div class="card">
+                    <div class="card-header">
+
+
+
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div>
+
+                            {{--friends posts--}}
+                            <div class="row frend-area">
+                                <h1>Whoops! Looks like something went wrong. If you believe something is wrong with FrendGrid please <a href="/support">let us know here.</a></h1>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                    <div class="card-footer">
+                        @include('partials.footerlinks')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
