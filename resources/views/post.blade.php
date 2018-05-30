38633468b53fd9ad26b4cb7cdb7c4221b02e0cae
NickSkye <nickskye7@gmail.com>
2018-05-29 18:40:58 -0700

    fixed users own page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -145,7 +145,7 @@
 
                                         @if (is_null($post->imagepath))
                                             {!! preg_replace('/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/', '<a href="$0">$0</a>', $post->description) !!}
-@php($rep = "hi";)
+@php($rep = "hi")
                                             <p>{!! preg_replace('/@([\w\-]+)/', '<a href="/users/$1">$0</a>', $rep )!!}</p>
                                             {{--<p class="post-data">views: {{$post->views}}</p>--}}
                                         @else
