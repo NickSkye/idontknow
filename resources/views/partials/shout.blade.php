87b6aa727f70cfc82e8bf7e2714083e11b4f5062
Jeff Yeon <jeff@wbrandstudio.com>
2018-05-31 09:58:05 -0700

    redid notification system

--- resources/views/partials/shout.blade.php
+++ resources/views/partials/shout.blade.php
@@ -1,5 +1,26 @@
 {{--start trial upload--}}
+<script>
+$(document).ready(function() {
+    src = "{{ route('searchajax') }}";
+    $("#search_text").autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                url: src,
+                dataType: "json",
+                data: {
+                    term : request.term
+                },
+                success: function(data) {
+                    response(data);
 
+                }
+            });
+        },
+        minLength: 3,
+
+    });
+});
+</script>
 <div class="container">
     <div class="panel panel-primary">
 
@@ -37,25 +58,3 @@
     </div>
 </div>
 
-<script>
-$(document).ready(function() {
-    src = "{{ route('searchajax') }}";
-    $("#search_text").autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: src,
-                dataType: "json",
-                data: {
-                    term : request.term
-                },
-                success: function(data) {
-                    response(data);
-
-                }
-            });
-        },
-        minLength: 3,
-
-    });
-});
-</script>
