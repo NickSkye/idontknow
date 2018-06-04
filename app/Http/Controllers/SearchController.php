f33f1c01b18fd51c1f36e047079ed5e115cfe594
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-04 10:39:58 -0700

    suggest

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -29,7 +29,7 @@ class SearchController extends Controller {
                 $location->longitude = 0;
         }
 
-        return DB::table('users')->select(DB::raw('*, ( 6367 * acos( cos( radians('.$location->latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$location->longitude.') ) + sin( radians('.$location->latitude.') ) * sin( radians( latitude ) ) ) ) AS distance'))->having('distance', '<', 5)->join('profileinfo', 'profileinfo.username', '=', 'users.username')->orderBy('distance')->get();
+        return DB::table('users')->select(DB::raw('*, ( 6367 * acos( cos( radians('.$location->latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$location->longitude.') ) + sin( radians('.$location->latitude.') ) * sin( radians( latitude ) ) ) ) AS distance'))->join('profileinfo', 'profileinfo.username', '=', 'users.username')->orderBy('distance')->limit(18)->get();
     }
 
     public function index(Request $request)
