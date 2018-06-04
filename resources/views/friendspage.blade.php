2e82c212c759c1ced45c807c13b31bb4fae8a755
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-04 16:21:36 -0700

    adding achievements

--- resources/views/friendspage.blade.php
+++ resources/views/friendspage.blade.php
@@ -267,7 +267,7 @@
 
                         <div class="achievements-mobile">
                             @foreach($achievements as $achievement)
-                                {{$achievement->achievement}}
+                                {{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}{{$achievement->achievement}}
                                 @endforeach
 
                         </div>
