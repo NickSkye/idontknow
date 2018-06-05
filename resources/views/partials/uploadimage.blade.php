179ab92f9b4faf2b2baec9be2915ed59698187e3
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-05 13:40:04 -0700

    adding achievements

--- resources/views/partials/uploadimage.blade.php
+++ resources/views/partials/uploadimage.blade.php
@@ -13,10 +13,10 @@
             <form action="{{ url('s3-image-upload') }}" enctype="multipart/form-data" method="POST">
                 {{ csrf_field() }}
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 text-center">
                         <input type="hidden" name="latitude" value=""/>
                         <input  type="hidden" name="longitude" value=""/>
-                        <img src="" id="profile-img-tag" style="width: 100%;" />
+                        <img src="" id="profile-img-tag" style="width: auto; max-height: 200px;" />
                         <textarea rows="4" cols="40" style="width: 100%;" placeholder="Whatcha thinkin about..." type="text" name="description" ></textarea>
                     </div>
                     <div class="col-6 offset-2 image-upload" style="align-self: flex-end;">
