a90b2420840fa9de7f6ca4b0208e63aa640db8d6
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-11 13:29:24 -0700

    added block unblock ability

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,7 +97,7 @@
                                     @endforeach
                                 </div>
                             </div>
-                            {{--{{$allwhoblocked->toArray()}}--}}
+                            {{$allwhoblocked}}
                             <div id="tab-2" class="tab-content">
                                 <div class="row frend-area">
 
