0b677d4c91f6f3bde3e9438bf0127f87fe676f1f
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-11 11:00:18 -0700

    added use loc var in settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-
+        view()->share('data', [1, 2, 3]);
 
 
     }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-10 col-sm-12 no-padding">
                 <div class="card">
                     <div class="card-header">
-
+{{$data}}
                         {{--@foreach($profileinfo as $item)--}}
                             <h2>{{$profileinfo->username}}</h2>
 
