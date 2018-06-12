d60e460d938c6ebc1173d5d0a61a9a44026dc0e6
NickSkye <nickskye7@gmail.com>
2018-06-12 11:15:46 -0700

    background location

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,8 @@
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0"/>
 
 
         <meta name="description" content="Your friends at your fingertips. Connect to frends all around you in real life."/>
