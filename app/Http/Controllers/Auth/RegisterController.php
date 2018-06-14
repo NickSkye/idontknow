7f16ddb38eff877efcaec07d3f6129a4937d27b2
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-14 10:25:14 -0700

    fixing first user interaction

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -35,7 +35,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/about';
 
     /**
      * Create a new controller instance.
