f622835822cc1cf019c2e908f82b187f42b477d6
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-14 10:22:30 -0700

    fixing first user interaction

--- resources/views/partials/shout.blade.php
+++ resources/views/partials/shout.blade.php
@@ -17,14 +17,14 @@
                     <input  type="hidden" name="longitude" value=""/>
                     <div class="col-12">
 
-                        <select id="sendtousername" name="sendtousername" style="width: 100%; height: 40px; font-size: 12pt; background-color: red; color: white;">
+                        <select id="sendtousername" name="sendtousername" style="width: 100%; height: 40px; font-size: 12pt; background-color: white; color: black; margin-bottom: 20px;">
                             @foreach($friends as $friend)
                             <option value="{{$friend->followsusername}}">{{$friend->followsusername}}</option>
                             @endforeach
                         </select>
                     </div>
                     <div class="col-12">
-                        <input class="shout-text" placeholder="Shout at your frend..." type="text" name="shout" style="width: 100%;" required>
+                        <input class="shout-text" placeholder="Shout at your frend..." type="text" name="shout" style="width: 100%; margin-bottom: 20px;" required>
                     </div>
                     <div class="col-12" style="align-self: flex-end;">
                         <button type="submit" class="btn shout-button modal-button" style="float: right;"><i aria-hidden="true" class="fa fa-bullhorn fa-2x"></i></button>
--- resources/views/partials/shoutonfriendspage.blade.php
+++ resources/views/partials/shoutonfriendspage.blade.php
@@ -17,7 +17,7 @@
                         <input type="hidden" name="sendtousername" value="{{$info->username}}"/>
                     </div>
                     <div class="col-12">
-                        <input  class="shout-text" placeholder="Shout at your frend..." type="text" name="shout" style="width: 100%;" required>
+                        <input  class="shout-text" placeholder="Shout at your frend..." type="text" name="shout" style="width: 100%; margin-bottom: 20px;" required>
                     </div>
                     <div class="col-12" style="align-self: flex-end;">
                         <button type="submit" class="btn shout-button modal-button" style="float: right;"><i aria-hidden="true" class="fa fa-bullhorn fa-2x"></i></button>
