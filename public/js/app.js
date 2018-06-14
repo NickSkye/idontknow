4bf34c48aa12d5d4df2e5f94036c76aaa6ca8436
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-14 16:57:53 -0700

    fixing first user interaction

--- public/js/app.js
+++ public/js/app.js
@@ -14266,6 +14266,7 @@ $("#remove_frend_form").submit(function (event) {
         $(".arent-frends").removeClass('d-none');
         $(".are-frends").addClass('d-none');
         $('#deleteModal').modal('toggle');
+        $('.add_frend_button').removeClass("d-none");
         // $("#server-results").html(response[0]);
         // if(response[1]){
         //     $("button.dislike").html('<i class="fa fa-thumbs-down fa-2x" style="color: blue;" aria-hidden="true"></i>');
