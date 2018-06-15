85ba4d82fb04e237acfb58ee25696b2a2d441125
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-15 16:23:05 -0700

    fixing first user interaction

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -269,13 +269,6 @@
 
                                         </div>
 
-
-                                            <div class="collapse multi-collapse" id="commentCollapse">
-                                                <div style="position: absolute; bottom: 30px; background: white; width: 100%; margin-left: -20px; height: 80px; padding: 10px;"> @include('partials.commentfield')</div>
-                                            </div>
-
-                                        
-
                                             @endauth
 
                                     </div>
@@ -287,6 +280,14 @@
                             </div>
 
                         </div>
+                            @auth
+                            <div class="card-footer">
+                                <div class="collapse multi-collapse" id="commentCollapse">
+                                    <div style="position: absolute; bottom: 30px; background: white; width: 100%; margin-left: -20px; height: 80px; padding: 10px;"> @include('partials.commentfield')</div>
+                                </div>
+
+                            </div>
+                            @endauth
                         <div class="comment-section infinite-scroll">
                             @foreach($thecomments as $comment)
                                 <div>
