86cce9713c92a2f4ea92fde1b5c355cafec86757
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-29 15:49:08 -0700

    fixing shout bug

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -31,13 +31,48 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="sendshoutModalLabel">Shout!</h5>
+                                <h5 class="modal-title" id="sendshoutModalLabel">Shout back!</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-                                @include('partials.shout')
+                                {{--start trial upload--}}
+
+                                <div class="container">
+                                    <div class="panel panel-primary">
+
+
+
+                                        <div class="panel-body">
+
+
+
+
+                                            <form action="{{ url('shouts/send') }}"  method="POST">
+                                                {{ csrf_field() }}
+                                                <div class="row">
+                                                    <input type="hidden" name="latitude" value=""/>
+                                                    <input  type="hidden" name="longitude" value=""/>
+                                                    <div class="col-12">
+                                                        <input  type="hidden" id="sendtousername" name="sendtousername" value=""/>
+
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <input class="shout-text" placeholder="Shout at your frend..." type="text" name="shout" style="width: 100%; margin-bottom: 20px;" required>
+                                                    </div>
+                                                    <div class="col-12" style="align-self: flex-end;">
+                                                        <button type="submit" class="btn shout-button modal-button" style="float: right;"><i aria-hidden="true" class="fa fa-bullhorn fa-2x"></i></button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+
                             </div>
                             {{--<div class="modal-footer">--}}
                             {{--<button type="button" class="btn btn-primary">Shout Back!</button>--}}
