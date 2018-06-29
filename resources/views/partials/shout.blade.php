6d11a7aaf0831510074955e8766509c38fd1ca36
Jeff Yeon <jeff@wbrandstudio.com>
2018-06-29 15:19:00 -0700

    fixing shout bug

--- resources/views/partials/shout.blade.php
+++ resources/views/partials/shout.blade.php
@@ -19,7 +19,7 @@
 
                         <select id="sendtousername" name="sendtousername" style="width: 100%; height: 40px; font-size: 12pt; background-color: white; color: black; margin-bottom: 20px;">
                             @foreach($friends as $friend)
-                            <option value="{{$friend->followsusername}}">{{$friend->followsusername}}</option>
+                            <option value="{{$friend}}">{{$friend}}</option>
                             @endforeach
                         </select>
                     </div>
