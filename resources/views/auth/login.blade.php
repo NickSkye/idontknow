e3b5d12f1aa336375459c43255a1d71b0a51bc57
NickSkye <nickskye7@gmail.com>
2018-07-23 21:26:17 -0700

    added fb

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,10 @@
                             </div>
 
                         </form>
-                        <div class="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="continue_with" data-show-faces="true" data-auto-logout-link="false" data-use-continue-as="true"></div>
+                        <div class="col-md-8 offset-md-4">
+                            <div class="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="continue_with" data-show-faces="true" data-auto-logout-link="false" data-use-continue-as="true"></div>
+                        </div>
+
                     </div>
                 </div>
             </div>
