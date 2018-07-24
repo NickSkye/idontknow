7880f6f7b203c702ef4fd13965bd57700ae76099
NickSkye <nickskye7@gmail.com>
2018-07-23 21:11:46 -0700

    added fb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,6 +125,24 @@
         @endguest
         @auth
         <body>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId            : '265358810920975',
+                    autoLogAppEvents : true,
+                    xfbml            : true,
+                    version          : 'v3.0'
+                });
+            };
+
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
         @endauth
 
         <div id="app">
