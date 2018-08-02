40cedef2d246f22b74ae011b92d1d646dacf2133
Jeff Yeon <jeff@wbrandstudio.com>
2018-08-02 10:21:20 -0700

    added dashboard

---
+++ resources/views/apps.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+<?php $page = 'about'; ?>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-sm-12 no-padding">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                    {{--<img src="{{ Session::get('path') }}">--}}
+                @endif
+
+                {{--MODAL FOR SHOUTS--}}
+
+
+
+                {{--END MODALS FOR SHOUTS--}}
+
+                <div class="card">
+                    <div class="card-header">
+                        @if(isset($_COOKIE['FG_User']))
+                        Hello {{$_COOKIE['FG_User']}}
+                        @endif
+                        The current date and time is:
+                        <div id="datetime"></div>
+
+
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                            <a href="/dashboard">Dashboard App</a>
+
+
+                    </div>
+                    <div class="card-footer">
+                        @include('partials.footerlinks')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -76,6 +76,9 @@
                             <a class="dropdown-item" href="/settings">
                                 {{ __('Settings') }}
                             </a>
+                            <a class="dropdown-item" href="/dashboard">
+                                {{ __('Dashboard') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -235,6 +238,9 @@
                                 </a>
                                 <a class="dropdown-item" href="/settings">
                                     {{ __('Settings') }}
+                                </a>
+                                <a class="dropdown-item" href="/dashboard">
+                                    {{ __('Dashboard') }}
                                 </a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
