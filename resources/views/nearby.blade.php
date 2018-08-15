35d7d3d4120f9fb0ff4a316b7f5972359ab81037
Jeff Yeon <jeff@wbrandstudio.com>
2018-08-15 12:14:15 -0700

    changed invite

--- resources/views/nearby.blade.php
+++ resources/views/nearby.blade.php
@@ -200,6 +200,8 @@
                                 @guest
                                 <h2 style="text-align: center;">Welcome to FrendGrid!</h2>
                                 <br>
+                                <h5><i class="fa fa-location-arrow fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;Your location is NEVER shared. Only a general radius is collected in order to see who is close while Protecting your Privacy.</h5>
+                                <br>
                                 <h5><i class="fa fa-globe fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;Connect with the world around you in real life</h5>
                                 <br>
                                 <h5><i class="fa fa-users fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;Make new friends based off people around you</h5>
