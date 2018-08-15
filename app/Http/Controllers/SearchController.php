3395f2a5499d93d7f517c16083282ccc04c5eeee
Jeff Yeon <jeff@wbrandstudio.com>
2018-08-15 11:11:00 -0700

    changed invite

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -81,19 +81,19 @@ class SearchController extends Controller {
         }
 
         if(is_numeric($request->email)) {
-            if (strlen($request->email) == 10) {
+
                 Nexmo::message()->send([
                     'to' => $request->email,
                     'from' => '19493403561',
                     'text' => 'Your friend ' . Auth::user()->name . ' wants to be friends with you on FrendGrid! Join Today at https://frendgrid.com/register !!! or download the mobile app for iOS at https://goo.gl/ygEdQZ !!!'
                 ]);
-            } else {
-                Nexmo::message()->send([
-                    'to' => '1' . $request->email,
-                    'from' => '19493403561',
-                    'text' => 'Your friend ' . Auth::user()->name . ' wants to be friends with you on FrendGrid! Join Today at https://frendgrid.com/register !!! or download the mobile app for iOS at https://goo.gl/ygEdQZ !!!'
-                ]);
-            }
+//            } else {
+//                Nexmo::message()->send([
+//                    'to' => '1' . $request->email,
+//                    'from' => '19493403561',
+//                    'text' => 'Your friend ' . Auth::user()->name . ' wants to be friends with you on FrendGrid! Join Today at https://frendgrid.com/register !!! or download the mobile app for iOS at https://goo.gl/ygEdQZ !!!'
+//                ]);
+//            }
 
         }
         else{
