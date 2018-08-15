e40c5a1a8dc2acf1ea2f96e2f7ee48f61a763db1
Jeff Yeon <jeff@wbrandstudio.com>
2018-08-15 11:54:44 -0700

    changed invite

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -124,7 +124,7 @@ class MessagesController extends Controller
             Nexmo::message()->send([
                 'to' => $phonenum,
                 'from' => '19493403561',
-                'text' => 'Your friend ' . Auth::user()->username . ' sent you a message https://frendgrid.com/shouts !'
+                'text' => 'You have a new shout https://frendgrid.com/shouts !'
             ]);
         }
         DB::table('users')->where('username', Auth::user()->username)->increment('score', 2);
