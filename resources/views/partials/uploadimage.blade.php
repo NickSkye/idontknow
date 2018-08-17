922d717a01e80f7606ad353456bbf40e011dd57e
NickSkye <nickskye7@gmail.com>
2018-08-16 21:51:04 -0700

    added localchat

--- resources/views/partials/uploadimage.blade.php
+++ resources/views/partials/uploadimage.blade.php
@@ -27,7 +27,7 @@
 
                     </div>
 
-                    <div class="col-md-4" style="align-self: flex-end;">
+                    <div class="col-md-12" style="align-self: flex-end;">
                         <button type="submit" class="btn upload-button modal-button post-button" style="float: right;" >Post</button>
                     </div>
                 </div>
