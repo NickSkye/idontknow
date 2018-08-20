003617c1ccea2a4683fb09505658ab3b50f09f07
Jeff Yeon <jeff@wbrandstudio.com>
2018-08-20 11:49:20 -0700

    loader

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -293,7 +293,7 @@ $(document).ready(function(){
         // $('.upload-button i.fa').hide();     //add the class to the clicked element
         //$('.upload-button i.fa').toggleClass("fa-spinner fa-pulse fa-fw");
         //$('.upload-button').prop("disabled",true);
-        $('.loader, loader-bg').css('display', 'block');
+        $('.loader, .loader-bg').css('display', 'block');
 
     });
 
