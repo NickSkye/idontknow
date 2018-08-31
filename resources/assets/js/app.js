21ea40934c5a298948c8d8ebd7a49bae11c971bc
NickSkye <nickskye7@gmail.com>
2018-08-31 00:43:42 -0700

    merge

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -398,7 +398,7 @@ $(document).ready(function(){
 //         swipe: true
 //
 //     });
-    $('.chat-box').scrollTop($('.chat-box').scrollHeight);
+    $('.chat-box').scrollTop($('.chat-box')[0].scrollHeight);
     $(".chat-box").get(0).scrollIntoView(false);
 
 });
