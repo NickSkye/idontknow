69ea42a0d1a95b612b16dc02ec6ef7d603976cfd
Jeff Yeon <jeff@wbrandstudio.com>
2018-09-12 12:34:36 -0700

    added ajax

--- public/js/app.js
+++ public/js/app.js
@@ -14142,7 +14142,7 @@ $(document).ready(function () {
         }).done(function (response) {
             //
 
-            var updated = "What You May Have Missed: <br> ";
+            var updated = "What You May Have Missed: <br><br> ";
             console.log(response[0][0]);
 
             $.each(response[0], function (k, v) {
